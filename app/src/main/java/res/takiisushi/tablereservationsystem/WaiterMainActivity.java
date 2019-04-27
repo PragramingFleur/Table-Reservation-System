3af6fc3714dfb4da2652a190872afa8345a724c6
PragramingFleur <fleur4296@gmail.com>
2019-04-27 13:55:55 +0200

    All: fixed dpi versions of all asset files
    Waiter Mobile:
    Created activity and layout
    finished layout
    set up Samsung S6 virtual device
    
    Tablet:
    slight changes in asset references
    
    Signed-off-by: PragramingFleur <fleur4296@gmail.com>

---
+++ app/src/main/java/res/takiisushi/tablereservationsystem/WaiterMainActivity.java
@@ -0,0 +1,13 @@
+package res.takiisushi.tablereservationsystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WaiterMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiter_main);
+    }
+}
