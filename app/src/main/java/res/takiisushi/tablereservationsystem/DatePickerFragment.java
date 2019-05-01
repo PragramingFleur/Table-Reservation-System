28b278a4d17f7df667eb733cb2a7da64ec6253f4
PragramingFleur <fleur4296@gmail.com>
2019-05-01 11:59:22 +0200

    Tablet: Change of Launcher icon to match Restaurant Added Date picker and changed necessary files

---
+++ app/src/main/java/res/takiisushi/tablereservationsystem/DatePickerFragment.java
@@ -0,0 +1,22 @@
+package res.takiisushi.tablereservationsystem;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, date);
+    }
+}
--- app/src/main/java/res/takiisushi/tablereservationsystem/MainActivity.java
+++ app/src/main/java/res/takiisushi/tablereservationsystem/MainActivity.java
@@ -1,13 +1,55 @@
 package res.takiisushi.tablereservationsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView datePickerButtonA = findViewById(R.id.calendarButton);
+        TextView datePickerButtonB = findViewById(R.id.dateViewer);
+
+        datePickerButtonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker a");
+            }
+        });
+
+        datePickerButtonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker b");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String currentDateString = DateFormat.getDateInstance(DateFormat.SHORT).format(calendar.getTime());
+
+        TextView dateTextView = findViewById(R.id.dateViewer);
+        dateTextView.setText(currentDateString);
     }
 }
