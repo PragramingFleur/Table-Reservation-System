3cd24465f6c4dd38325243b35c2de8aeb5859ea3
PragramingFleur <fleur4296@gmail.com>
2019-05-18 18:48:31 +0200

    Tried but failed to save and restore rearrangeable layout from save.
    Signed-off-by: PragramingFleur <fleur4296@gmail.com>

--- app/src/main/java/res/takiisushi/tablereservationsystem/RestaurantLayoutMainActivity.java
+++ app/src/main/java/res/takiisushi/tablereservationsystem/RestaurantLayoutMainActivity.java
@@ -1,5 +1,6 @@
 package res.takiisushi.tablereservationsystem;
 
+import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +11,13 @@ import android.view.View;
 
 import com.rajasharan.layout.RearrangeableLayout;
 
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class RestaurantLayoutMainActivity extends AppCompatActivity {
     private static final String TAG = "REST-REARRANGEABLE-LOUT";
-    private RearrangeableLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,17 @@ public class RestaurantLayoutMainActivity extends AppCompatActivity {
 
         setContentView(R.layout.restaurant_main);
 
-        root = findViewById(R.id.restaurant_layout);
+        final RearrangeableLayout root = findViewById(R.id.restaurant_layout);
         root.setChildPositionListener(new RearrangeableLayout.ChildPositionListener() {
             @Override
             public void onChildMoved(View childView, Rect oldPosition, Rect newPosition) {
                 Log.d(TAG, childView.toString());
                 Log.d(TAG, oldPosition.toString() + " -> " + newPosition.toString());
+                saveTable(root.getContext(), childView);
             }
         });
+
+        getSavedTables(root);
     }
 
     @Override
@@ -37,5 +45,70 @@ public class RestaurantLayoutMainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.example_menu, menu);
         return true;
+    }
+
+    public void saveTable(Context context, View viewTable) {
+        try {
+            // create a new file with an ObjectOutputStream
+            FileOutputStream out = context.openFileOutput("tables", MODE_APPEND);
+            ObjectOutputStream oout = new ObjectOutputStream(out);
+
+            // write something in the file
+            oout.writeObject(viewTable);
+            oout.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getSavedTables(RearrangeableLayout parent) {
+        ArrayList<View> al = new ArrayList<>();
+        boolean cont = true;
+        try {
+            // create an ObjectInputStream for the file we need to read from
+            ObjectInputStream ois = new ObjectInputStream(parent.getContext().openFileInput("tables"));
+
+            // read objects from file
+            while (cont) {
+                View view = null;
+                try {
+                    view = (View) ois.readObject();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                if (view != null)
+                    al.add(view);
+                else
+                    cont = false;
+            }
+
+            if (!al.isEmpty()) {
+                View childView;
+
+                for (int count = 0; count <= al.size() - 1; count++) {
+                    for (int counter = 0; counter <= parent.getChildCount() - 1; counter++) {
+                        childView = parent.getChildAt(counter);
+
+                        //check if view on file is same as child view
+                        if (getResources().getResourceEntryName(parent.getChildAt(counter).getId()) == getResources().getResourceEntryName(al.get(count).getId())) {
+                            //check layout parameters are same or different
+                            if (al.get(count).getLayoutParams() != parent.getChildAt(counter).getLayoutParams()) {
+                                parent.removeView(childView);
+                                parent.addView(al.get(count));
+                                /*parent.getChildAt(counter).setLeft(al.get(count).getLeft());
+                                parent.getChildAt(counter).setTop(al.get(count).getTop());
+                                parent.getChildAt(counter).setRight(al.get(count).getRight());
+                                parent.getChildAt(counter).setBottom(al.get(count).getBottom());*/
+                            }
+                        }
+
+                    }
+                }
+            }
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
