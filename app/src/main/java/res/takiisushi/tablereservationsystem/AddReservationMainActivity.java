9a929a40e7b9e9a8f5cd8fe670cf11c1bd123d2f
PragramingFleur <fleur4296@gmail.com>
2019-05-25 14:16:28 +0200

    AddReservationMainActivity:   Checks form if the required fields are filled out and correctly when reserve button is pressed
    Signed-off-by: PragramingFleur <fleur4296@gmail.com>

--- app/src/main/java/res/takiisushi/tablereservationsystem/AddReservationMainActivity.java
+++ app/src/main/java/res/takiisushi/tablereservationsystem/AddReservationMainActivity.java
@@ -12,11 +12,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class AddReservationMainActivity extends AppCompatActivity
         implements TimePickerDialog.OnTimeSetListener,
@@ -25,6 +37,7 @@ public class AddReservationMainActivity extends AppCompatActivity
     private static final String TAG = "ADD-RESERVATION";
     String tableSelected;
     int spinnerId = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,131 @@ public class AddReservationMainActivity extends AppCompatActivity
         setupDialogs();
 
         createDeleteTableSpinner(root);
+
+        //Button and click listener for when add Reservation is clicked
+        Button addReservationButton = findViewById(R.id.reserveButton);
+
+        addReservationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isCorrect = checkFields();
+                if (isCorrect == true) {
+                    //addReservationToDB();
+                }
+            }
+        });
+    }
+
+    private boolean checkFields() {
+        List<Boolean> isCorrectCollection = new ArrayList<>();
+        boolean isCorrect;
+
+        EditText editNumber = findViewById(R.id.editMobileNum);
+        TextView editDate = findViewById(R.id.selectDateNewReservation);
+        TextView editTime = findViewById(R.id.selectTimeNewReservation);
+        EditText editAdultGuestNum = findViewById(R.id.editAdultGuestNum);
+        EditText editChildGuestNum = findViewById(R.id.editChildrenGuestNum);
+        List<String> tableList = Arrays.asList(tableSelected.split(","));
+        Calendar c = Calendar.getInstance();
+        Date today = c.getTime();
+
+        //Check if required fields are filled out
+        if (editNumber.getText().toString().trim().length() < 8) {
+            //Number field is empty or less than 8digits which is wrong
+            isCorrectCollection.add(false);
+            editNumber.setError("Invalid Number");
+        } else if (editNumber.getText().toString().trim().isEmpty()) {
+            isCorrectCollection.add(false);
+            editNumber.setError("Required");
+        } else if (!editDate.getText().toString().isEmpty()) {
+            //If date textview is not empty then check if the date is before today
+            Date strDate = null;
+            try {
+                strDate = new SimpleDateFormat("yyyy/MM/dd").parse(editDate.getText().toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if (strDate.before(today)) {
+                //date isn't supposed to be before today
+                isCorrectCollection.add(false);
+                editDate.setError("Date has to be today or after");
+            } else {
+                isCorrectCollection.add(true);
+            }
+
+        } else if (editDate.getText().toString().isEmpty()) {
+            isCorrectCollection.add(false);
+            editDate.setError("Required");
+        } else if (!editTime.getText().toString().isEmpty()) {
+            //Check if time is between 13:00 and 22:00
+            String selectedTime = editTime.getText().toString();
+            String minTime = "13:00";
+            String maxTime = "22:00";
+
+            Date selectedTimeConverted = null;
+            Date minTimeConverted = null;
+            Date maxTimeConverted = null;
+            try {
+                selectedTimeConverted = new SimpleDateFormat("HH:mm").parse(selectedTime);
+                minTimeConverted = new SimpleDateFormat("HH:mm").parse(minTime);
+                maxTimeConverted = new SimpleDateFormat("HH:mm").parse(maxTime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Calendar calendar = Calendar.getInstance();
+            Calendar calendarA = Calendar.getInstance();
+            Calendar calendarB = Calendar.getInstance();
+
+            calendar.setTime(selectedTimeConverted);
+            calendarA.setTime(minTimeConverted);
+            calendarB.setTime(maxTimeConverted);
+
+            if (calendar.getTime().after(calendarA.getTime()) && calendar.getTime().before(calendarB.getTime())) {
+                isCorrectCollection.add(true);
+            } else {
+                isCorrectCollection.add(false);
+                editTime.setError("Time is not between 13:00 and 22:00");
+            }
+
+        } else if (editTime.getText().toString().isEmpty()) {
+            //Check if time is empty
+            isCorrectCollection.add(false);
+            editTime.setError("Required");
+        } else if (Integer.parseInt(editAdultGuestNum.getText().toString()) == 0) {
+            //check if guest number is 0
+            isCorrectCollection.add(false);
+            editAdultGuestNum.setError("Guest Number should be 1 or Above");
+        } else if (Integer.parseInt(editAdultGuestNum.getText().toString()) > 0) {
+            isCorrectCollection.add(true);
+        } else if (Integer.parseInt(editChildGuestNum.getText().toString()) == 0) {
+            //check if guest number is 0
+            isCorrectCollection.add(false);
+            editAdultGuestNum.setError("Guest Number should be 1 or Above");
+        } else if (Integer.parseInt(editChildGuestNum.getText().toString()) > 0) {
+            isCorrectCollection.add(true);
+        } else if (!tableSelected.isEmpty()) {
+            //Checking for duplicate tables
+            if (tableList.size() > 1) {
+                final Set<Integer> set = new HashSet<>();
+
+                for (String string : tableList) {
+                    if (!set.add(Integer.parseInt(string))) {
+                        TextView tableWarning = findViewById(R.id.tableTextView);
+                        tableWarning.setError("Cannot Repeat Table number");
+                        isCorrectCollection.add(false);
+                        break;
+                    } else {
+                        isCorrectCollection.add(true);
+                    }
+                }
+            }
+        } else {
+            isCorrectCollection.add(true);
+        }
+
+        isCorrect = !isCorrectCollection.contains(false);
+
+        return isCorrect;
     }
 
     private void setupActionBar() {
@@ -82,7 +220,7 @@ public class AddReservationMainActivity extends AppCompatActivity
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 newSpinner.setAdapter(adapter);
                 newSpinner.setId(spinnerId);
-                spinnerId += 1;
+                spinnerId = spinnerId + 1;
                 newSpinner.setOnItemSelectedListener(listener);
                 layout.addView(newSpinner);
             }
@@ -129,6 +267,7 @@ public class AddReservationMainActivity extends AppCompatActivity
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
         LinearLayout superParent = (LinearLayout) parent.getParent();
         int childCount = superParent.getChildCount();
         tableSelected = null;
